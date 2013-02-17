4896524d6d589e447bd779b59de017b845a2bba6
Cezar Berea <berea.cezar@gmail.com>
2013-02-17 02:25:13 +0200

    More UI work

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,24 @@
-console.log('This would be the main JS file.');
+$(function(){
+
+  var $nav = $("#nav"),
+      $header = $("#header");
+
+  $(document).scrollspy({
+    min: function() {
+      return $nav.offset().top;
+    },
+    onEnter: function(element, position) {
+      $header.addClass("fixed");
+    }
+  });
+
+  $(document).scrollspy({
+    max: function() {
+      return $header.height();
+    },
+    onEnter: function(element, position) {
+      $header.removeClass("fixed");
+    }
+  });
+
+});
